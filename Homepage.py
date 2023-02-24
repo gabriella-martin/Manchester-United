1aeca3cf51edfe7810bf59aeaea50348b36f30a1
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-02-24 15:07:40 +0000

    attempt postgres conn

--- Homepage.py
+++ Homepage.py
@@ -23,7 +23,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from players;")
 
 # Print results.
 for row in rows:
